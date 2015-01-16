e6337a33fdbfcc517bbc53d491b1a54cfdec3bf6
paveytel <paveytel@buffalo.edu>
2015-01-16 15:27:15 -0500

    created a livecard and a remote view which were initialized in onStartCommand()

--- app/src/main/java/com/paveynganpi/gohunterforglass/LiveCardService.java
+++ app/src/main/java/com/paveynganpi/gohunterforglass/LiveCardService.java
@@ -3,8 +3,16 @@ package com.paveynganpi.gohunterforglass;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.RemoteViews;
+
+import com.google.android.glass.timeline.LiveCard;
 
 public class LiveCardService extends Service {
+
+    LiveCard mLiveCard;
+    RemoteViews mRemoteViews;
+    private static final String TAG = "GoHunt For Glass Status";
+
     public LiveCardService() {
     }
 
@@ -12,5 +20,16 @@ public class LiveCardService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if(mLiveCard == null){
+            mRemoteViews = new RemoteViews(getPackageName(),R.layout.status_live_card_layout);
+            mLiveCard = new LiveCard(this,TAG);
+
+            mLiveCard.setViews(mRemoteViews);
+        }
+        return START_STICKY;//keeps it running
     }
 }
