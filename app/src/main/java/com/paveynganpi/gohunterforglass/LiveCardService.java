26022ec29163b666252c7d995ba91063c341bc93
paveytel <paveytel@buffalo.edu>
2015-01-17 00:38:51 -0500

    voice trigger resource file

--- app/src/main/java/com/paveynganpi/gohunterforglass/LiveCardService.java
+++ app/src/main/java/com/paveynganpi/gohunterforglass/LiveCardService.java
@@ -1,5 +1,6 @@
 package com.paveynganpi.gohunterforglass;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -12,6 +13,7 @@ public class LiveCardService extends Service {
     LiveCard mLiveCard;
     RemoteViews mRemoteViews;
     private static final String TAG = "GoHunt For Glass Status";
+    private static final String ACTION_STOP = "Stop";
 
     public LiveCardService() {
     }
@@ -23,12 +25,40 @@ public class LiveCardService extends Service {
     }
 
     @Override
+    public void onDestroy() {
+        if(mLiveCard != null){
+            mLiveCard.unpublish();//takes us out of the timeline
+        }
+        super.onDestroy();
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if(mLiveCard == null){
             mRemoteViews = new RemoteViews(getPackageName(),R.layout.status_live_card_layout);
             mLiveCard = new LiveCard(this,TAG);
 
             mLiveCard.setViews(mRemoteViews);
+
+            Intent cardActionIntent = new Intent(this,LiveCardService.class);//create an intent for this service
+            cardActionIntent.setAction(ACTION_STOP);
+            mLiveCard.setAction(PendingIntent.getService(this, 0, cardActionIntent, 0));
+
+            mLiveCard.publish(LiveCard.PublishMode.REVEAL);//add live card to timeline and take user to it
+
+
+        }
+        else {
+
+            if(ACTION_STOP == intent.getAction()){
+                stopSelf();
+            }
+            else{
+
+                mLiveCard.navigate();
+
+            }
+
         }
         return START_STICKY;//keeps it running
     }
